837ca31c4c17b700096598f81b7a578d5dd4faa9
Sergio_Robles <sroblesg@miumg.edu.gt>
2024-08-18 19:13:27 -0600

    cambios en el slide

--- funciones/slide.js
+++ funciones/slide.js
@@ -1,6 +1,5 @@
-
-let slides = []
-let contenedorSlide = document.querySelectorAll('.slide');
+let slides = [];
+let contenedorSlide = document.querySelector('.slide');
 let fotosSlide;
 
 class Slide {
@@ -10,25 +9,15 @@ class Slide {
 }
 
 let foto1 = new Slide("AMARILLO.jpg");
-
 let foto2 = new Slide('imagenes/FOTOS-CARRUSEL/call-center.jpg');
-
 let foto3 = new Slide('imagenes/FOTOS-CARRUSEL/informacion.jpg');
-
 let foto4 = new Slide('imagenes/FOTOS-CARRUSEL/cei.jpg');
 
-
-
-slides.push(foto1,foto2,foto3,foto4);
-
+slides.push(foto1, foto2, foto3, foto4);
 
 slides.forEach((fot) => {
-
     fotosSlide = `<div class="item-slide">
-  <img src="${fot.url}"  alt="">
-</div>`
-
-contenedorSlide.innerHTML += fotosSlide;
+  <img src="${fot.url}" alt="">
+</div>`;
+    contenedorSlide.innerHTML += fotosSlide;
 });
-
-
